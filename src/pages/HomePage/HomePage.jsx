6a05b4a28caec4b986e9440205107e68f17bd6b7
avilla13 <xetox13@gmail.com>
2023-08-25 12:03:22 -0500

    Update HomePage to render aiWod conditionally and re-style with previous HomePage.css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import './HomePage.css';
 import * as aiwodsApi from '../../utilities/aiwods-api';
 
 export default function HomePage({ user }) {
-  const [aiWod, setAiWod] = useState({});
+  const [aiWod, setAiWod] = useState(null);
 
   async function handleClick(){
     const response = await aiwodsApi.createAIWod();
@@ -17,15 +17,21 @@ export default function HomePage({ user }) {
       <h1>Home WOD</h1>
       <div className='home-container'>        
         <div className='wod-ai'>
-          <h2>{ aiWod.type }</h2>
-          { aiWod.rounds ? `${aiWod.rounds} Rounds of:` : `in ${aiWod.duration} minutes:` } <br />
-          <ul>
-            {aiWod.movements.map((movement, idx) => (
-              <li key={idx} >
-                {movement.reps} {movement.movement} {movement.weight ? `(${movement.weight})` : `` }
-              </li>
-            ))}
-          </ul>
+          { aiWod ?
+          <>
+            <h2>{ aiWod.type }</h2>
+            { aiWod.rounds ? `${aiWod.rounds} Rounds of:` : `in ${aiWod.duration} minutes:` } <br />
+            <ul>
+              {aiWod.movements && aiWod.movements.map((movement, idx) => (
+                <li key={idx} >
+                  {movement.reps} {movement.movement} {movement.weight ? `(${movement.weight})` : `` }
+                </li>
+              ))}
+            </ul>
+          </>
+          :
+          <h3>Click on button to Generate new WOD</h3>
+          }
         </div>
         <button onClick={handleClick} >Generate WOD</button>
       </div>
